6f52e606f010ef52b370283a7946c3666b0120d2
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 11:08:04 +0530

    Updates

---
+++ precache-manifest.d13c3d241844574591d8c76ef51e98d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055d743dd2567d38f898dceeb51c3050",
+    "url": "/lifetime/index.html"
+  },
+  {
+    "revision": "f22466650dfbad1da164",
+    "url": "/lifetime/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/lifetime/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "f22466650dfbad1da164",
+    "url": "/lifetime/static/js/main.a5b7c456.chunk.js"
+  },
+  {
+    "revision": "206294c0a561623ab2d5",
+    "url": "/lifetime/static/js/runtime~main.fc20cf16.js"
+  }
+]);
